fa1fc1f1934f37a21bb90ce017eff6f0617eb024
Chase Hardin <chase.hardin@Chases-MacBook-Pro-2.local>
2017-05-26 20:35:02 -0500

    Setup initial routing. Add top-nav component.

--- MyInventory.Web/src/app/app-routing.module.ts
+++ MyInventory.Web/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from "./features/home/home.component";
 
 const routes: Routes = [
   {
-    path: '',
-    children: []
+    path: 'home', component: HomeComponent
   }
 ];
 
--- MyInventory.Web/src/app/app.module.ts
+++ MyInventory.Web/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './features/home/home.component';
+import { TopNavComponent } from './features/top-nav/top-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TopNavComponent
   ],
   imports: [
     BrowserModule,
